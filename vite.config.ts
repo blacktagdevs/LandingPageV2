2e550fd81950537321d3ed8e1bd44ad3f73f894f
Jamar Mitchell <jamarkeanumitchell@gmail.com>
2025-10-12 11:05:33 -0400

    changes for react

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/LandingPageV2/",
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
